61936c46712391669ac03dd96ed0256effb75a46
TopFirmino <furcas.lorenzo@gmail.com>
2025-10-29 19:45:09 +0100

    Tz organizer is now public in order to use tzen programmatically

--- tzen/__main__.py
+++ tzen/__main__.py
@@ -1,8 +1,9 @@
 from ._tz_cli import app
 from ._tz_logging import tz_getLogger
 
-# Configure the root logger for the tzen application
-logger = tz_getLogger( __name__)
+
 
 if __name__ == "__main__":
+    # Configure the root logger for the tzen application
+    logger = tz_getLogger(__name__)
     app()
